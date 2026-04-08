5bcf70cd3d358c5868371359de4c6246d2bb4e42
rameesabava <rameesabava91@gmail.com>
2026-04-08 12:25:23 +0530

    footer designed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { FaArrowRight, FaEnvelope, FaFacebookSquare, FaInstagram } from 'react-icons/fa'
+import { FaXTwitter } from 'react-icons/fa6'
+
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='w-full bg-black text-white p-5'>
+      <div className='md:grid grid-cols-3'>
+        <div className='mx-5 my-5'>
+          <h1 className='mb-3 text-2xl'>ABOUT US</h1>
+          <p className='text-justify'>We believe books are more than just pages - they are windows into new worlds, teachers of wisdom, and companions for life. our journey began with a passion for storytelling and a mission to make reading accessible,enjoyable, and inspiring for everyone.</p>
+        </div>
+        <div className='mx-5 my-5'>
+          <h1 className='mb-3 text-2xl'>NEWS LETTER</h1>
+          <p>Stay updated with our latest trends</p>
+          <div className='flex items-center'>
+            <input type="text" className='bg-white my-3 text-black p-1' placeholder='E Mail' />
+            <div className='bg-yellow-500 p-2'> <FaArrowRight /> </div>
+          </div>
+        </div>
+        <div className='mx-5 my-5'>
+          <h1 className='mb-3 text-2xl'>FOLLOW US</h1>
+          <p>Let us be social</p>
+          <div className='flex items-center gap-5 my-3'>
+            <FaInstagram />
+            <FaXTwitter className='mx-1' />
+            <FaFacebookSquare />
+            <FaEnvelope />
+          </div>
+        </div>
+      </div>
+      <p className='text-center'>Copyright &copy; 2026 All rights reserved | This website is made with By Luminar Technolab</p>
+    </div>
+
   )
 }
 
--- src/user/pages/Contact.jsx
+++ src/user/pages/Contact.jsx
@@ -16,7 +16,7 @@ function Contact() {
             <div style={{ width: '50px', height: '50px', borderRadius: '50%' }} className='flex items-center justify-center bg-gray-200'>
               <FaLocationPin />
             </div>
-            <p className='ms-5'>123 Main Street, Apt 4B, Anytown, CA 91234</p>
+            <p className='mx-5'>123 Main Street, Apt 4B, Anytown, CA 91234</p>
 
           </div>
           <div className='flex items-center my-3 md:my-0'>
@@ -49,14 +49,14 @@ function Contact() {
                 <input placeholder='Message' type="text" className='bg-white w-full p-2' />
               </div>
               <div className='my-5'>
-<button className='bg-black p-2 w-full text-white text-lg flex justify-center items-centerz'>Submit <FaPaperPlane className='ms-2'/></button>
+                <button className='bg-black p-2 w-full text-white text-lg flex justify-center items-centerz'>Submit <FaPaperPlane className='ms-2' /></button>
               </div>
 
             </form>
 
           </div>
           <div className='mt-5 md:mt-0'>
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3928.984172422061!2d76.34009657369701!3d10.01816439008814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b080ffce877d5ef%3A0x8bef6870ad11b98!2sLuminar%20Technolab%20-%20Python%2C%20Data%20Science%2C%20AI%2C%20ASP.NET%2C%20Flutter%2C%20Software%20Testing%20Training%20Institute%20in%20Kochi!5e0!3m2!1sen!2sin!4v1775627514791!5m2!1sen!2sin" width="100%" height="450" style={{border:'0'}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3928.984172422061!2d76.34009657369701!3d10.01816439008814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b080ffce877d5ef%3A0x8bef6870ad11b98!2sLuminar%20Technolab%20-%20Python%2C%20Data%20Science%2C%20AI%2C%20ASP.NET%2C%20Flutter%2C%20Software%20Testing%20Training%20Institute%20in%20Kochi!5e0!3m2!1sen!2sin!4v1775627514791!5m2!1sen!2sin" width="100%" height="450" style={{ border: '0' }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
           </div>
 
         </div>
